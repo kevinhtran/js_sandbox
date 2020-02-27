5f0ba119c5f85e182c9a1e4e90262b21da11e215
kevinhtran <kevinhtran@gmail.com>
2020-02-26 17:38:23 -0800

    working with DOM selectors for multiple elements

--- app.js
+++ app.js
@@ -1,75 +1,67 @@
 // Section 3: DOM Manipulation & Events
 
-// DOM Selectors For Single Elements
+// DOM Selectors for Multiple Elements
+// Used to select more than one element
+// Returns either a HTML collection or node list which are both similar to array, except there are certain things you can't do with normal arrays.
+// Although they can be converted to arrays very easily.
 
-// document object methods that allow us to pull things from the DOM and then we can do different things with the elements/node
-// vanilla js
+// document.getElementsByClassName
 
-//single element selectors
-  // will allow you to grab a single element by its id or class, etc...
-  // stores one thing only
-  // grabs the single first one
+const items = document.getElementsByClassName('collection-item'); // fetch anything with this class name
+console.log(items); // prints out the HTML collection and within it, you'll see all the different properties, ids, classes... etc
+console.log(items[0]); // get the first list item within the HTML collection
+items[0].style.color = 'red'; // we can change style by indexing them just like an array
+items[3].textContent = 'Hello World'; // we can access them just like an array and change the text
 
+const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
 
-// multiple element selectors
-  // grabs all the elements of that class and node list
-
-
-// document.getElementById()
-// selects things by their id
-
-console.log(document.getElementById('task-title'));
-
-// Get things from the element
-
-console.log(document.getElementById('task-title').id); //get ele by id
-console.log(document.getElementById('task-title').className); // by class
-
-// // Change styling
-// document.getElementById('task-title').style.background = '#333'; // change the background to a certain color.
-// document.getElementById('task-title').style.color = '#fff'; // change to white
-// document.getElementById('task-title').style.padding = '5px'; // change padding
-// // document.getElementById('task-title').style.display = 'none'; // completely get rid of display
-// // this is to show you that you can change styles in JS but use CSS instead.
-
-// // Change content
-// // 
-// document.getElementById('task-title').textContent = 'Task List'; //change context of the list
-// document.getElementById('task-title').innerText = 'My Tasks'; // change inner contents of inner text
-// document.getElementById('task-title').innerHTML = '<span style ="color:red">Task List</span>'; // you can see that you are not changing the style inside of the span tag
-
-// it's not very efficient, so here's what we do, we set it to a variable:
-const taskTitle = document.getElementById('task-title');
-
-//Change styling
-taskTitle.style.background = '#333';
-taskTitle.style.color = '#fff';
-taskTitle.style.padding = '5px';
-// taskTitle.style.display = 'none';
-
-// Change content
-taskTitle.textContent = 'Task List';
-taskTitle.innerText = 'My Tasks';
-taskTitle.innerHTML = '<span style="color:red">Task List</span>';
-
-// document.querySelector()
-// newer and much more powerful
-// you don't have to select things by id, you can select them by... anything
-// basically works like jQuery because you can put any CSS selectors in here
-
-console.log(document.querySelector('#task-title'));
-console.log(document.querySelector('.card-title'));
-console.log(document.querySelector('h5')); // if there's more than one on the page, you're going to get the first one.
-
-// for li items
-document.querySelector('li').style.color = 'red'; // single element selector, so it only gets the first one
-document.querySelector('ul li').style.color = 'blue'; // you can also do nested elements
-document.querySelector('li:last-child').style.color = 'red'; // you can also use css pseudo code in here
-document.querySelector('li:nth-child(3)').style.color = 'yellow'; // you can also use the nth child pseudo code to select certain li items
-document.querySelector('li:nth-child(4)').textContent = 'Hello World'; // you can also change the text for that item
-document.querySelector('li:nth-child(odd)').style.background = '#ccc'; // you'll notice that only the first odd has been selected because this is a single element selector
-document.querySelector('li:nth-child(even)').style.background = '#f4f4f4'; // same thing for even, it only selects one
+console.log(listItems);
 
 
+// // document.getElementsByTagName
+let lis = document.getElementsByTagName('li');
+console.log(lis);
+console.log(lis[0]);
+lis[0].style.color = 'red';
+lis[3].textContent = 'Hello World';
+
+// // Convert HTML Collection into an Array and use it just like an array
+lis = Array.from(lis);
+
+lis.reverse();
+
+lis.forEach(function(li, index){
+  console.log(li.className)
+  li.textContent = `${index}: Hello`;
+});
+
+console.log(lis);
+
+// document.querySelectorAll
+// Pretty much like document.getElements and converting it into an HTML collection, except that it returns what is called a node list
+// a node list counts not just elements but things like 'text nodes'. Run a forEach without converting it to an array
+
+const items = document.querySelectorAll('ul.collection li.collection-item');
+
+// we're iterating through our items variable which is a querySelectorAll and doesn't need you to convert it to an array.
+items.forEach(function(li, index) {
+  li.textContent = `${index}: Hello`;
+});
+
+// using querySelectorAll with certain css pseudo code and setting it to const
+const liOdd = document.querySelectorAll('li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even)');
+
+// with a .forEach()
+liOdd.forEach(function(li, index) {
+  li.style.background = "#f4f4f4";
+});
+
+// with a for loop
+for (let i = 0; i < liEven.length; i++) {
+  liEven[i].style.background = "#ccc";
+}
+
+console.log(items);
 
 
